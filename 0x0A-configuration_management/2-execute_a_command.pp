7b6a68e4ba648fe578c758c2cfc5c82ee538a5c1
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-09 18:13:48 -0500

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Execute a command Using Puppet
 exec {'killmenow_process':
-  path => ['/bin/bash'],
-  command => "pkill killmenow"
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "pkill killmenow",
 }
